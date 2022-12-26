06bc2573f1709e76ca654ce73223365e2fb7d37b
patelaftab <10patelaftab@gmail.com>
2022-12-26 19:06:27 +0530

    [Aftab] Finding Maximum Number

---
+++ GenericUCs/Program.cs
@@ -0,0 +1,18 @@
+﻿using GenericUCs;
+
+namespace GeneicUCs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstvalue = 40;
+            int secondvalue =20;
+            int thirdvalue = 30;
+
+            MaximumNumber maximumNumber = new MaximumNumber();
+            int result = maximumNumber.MaximumIntergerNumber(firstvalue,secondvalue,thirdvalue);
+            Console.WriteLine(result);
+        }
+    }
+}
